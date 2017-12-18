bcf833225b6d093bbf06f70f92cfb220b6aff309
Ahmed Zubair <mac@Macs-MacBook-Pro.local>
2017-12-17 22:00:34 -0500

    asd

--- app/src/main/java/com/vlogellaa/espresso/ufl/ui/fragments/RecycleAdapter.java
+++ app/src/main/java/com/vlogellaa/espresso/ufl/ui/fragments/RecycleAdapter.java
@@ -1,7 +1,14 @@
 package com.vlogellaa.espresso.ufl.ui.fragments;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.vlogellaa.espresso.ufl.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class RecycleAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
@@ -19,4 +26,17 @@ public class RecycleAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolde
     public int getItemCount() {
         return 0;
     }
+
+    static class DatesViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.day_text)
+        TextView dayText;
+
+        public DatesViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+
 }
