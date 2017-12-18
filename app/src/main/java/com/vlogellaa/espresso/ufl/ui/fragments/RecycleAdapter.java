4bedc5473b92b442270132d9110b07105cafd91e
Ahmed Zubair <mac@Macs-MacBook-Pro.local>
2017-12-17 21:59:25 -0500

    asd

---
+++ app/src/main/java/com/vlogellaa/espresso/ufl/ui/fragments/RecycleAdapter.java
@@ -0,0 +1,22 @@
+package com.vlogellaa.espresso.ufl.ui.fragments;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+public class RecycleAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/vlogellaa/espresso/ufl/ui/fragments/bottom_nav/FixtureFragment.java
+++ app/src/main/java/com/vlogellaa/espresso/ufl/ui/fragments/bottom_nav/FixtureFragment.java
@@ -4,7 +4,6 @@ package com.vlogellaa.espresso.ufl.ui.fragments.bottom_nav;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
